e01d992ce99bd56a1835a1b49e6a192eb14386e5
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-17 00:56:24 +0100

    Add method to get nightlyRate

--- src/main/java/BedroomType.java
+++ src/main/java/BedroomType.java
@@ -1,16 +1,22 @@
 public enum BedroomType {
 
-    SINGLE(1),
-    DOUBLE(2),
-    FAMILY(3);
+    SINGLE(1, 40),
+    DOUBLE(2, 60),
+    FAMILY(3, 70);
 
     private final int capacity;
+    private final int nightlyRate;
 
-    BedroomType(int capacity) {
+    BedroomType(int capacity, int nightlyRate) {
         this.capacity = capacity;
+        this.nightlyRate = nightlyRate;
     }
 
     public int getCapacity() {
         return this.capacity;
+    }
+
+    public int getNightlyRate() {
+        return this.nightlyRate;
     }
 }
