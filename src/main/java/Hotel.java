7788e5c912c71e20615191df591979e51cef49a2
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-17 01:40:27 +0100

    Add method related to DiningRoom hashmap in Hotel class

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Hotel {
 
     private final ArrayList<Bedroom> bedroomCollection;
     private final ArrayList<ConferenceRoom> conferenceRoomCollection;
+    private final HashMap<String, DiningRoom> diningRoomCollection;
 
     public Hotel() {
         this.bedroomCollection = new ArrayList<>();
         this.conferenceRoomCollection = new ArrayList<>();
+        this.diningRoomCollection = new HashMap<>();
     }
 
     public ArrayList<Bedroom> getBedroomCollection() {
@@ -36,5 +39,17 @@ public class Hotel {
 
     public Booking bookRoom(int daysBooked, Bedroom bedroom) {
         return new Booking(daysBooked, bedroom);
+    }
+
+    public HashMap<String,DiningRoom> getDiningRooms() {
+        return this.diningRoomCollection;
+    }
+
+    public void addDiningRoom(DiningRoom diningRoom1) {
+        this.diningRoomCollection.put(diningRoom1.getName(), diningRoom1);
+    }
+
+    public int getDiningRoomsNumber() {
+        return this.diningRoomCollection.size();
     }
 }
